339e776114fd416dbf09dc233485ed8832426b6c
Alexey Brovko <alexey.brovko@jag.se>
2024-12-30 18:43:18 +0200

    Fix RS1038

--- Refit.Analyzer/Refit.Analyzer/Refit.Analyzer.Sample/IWrongInterface.cs
+++ Refit.Analyzer/Refit.Analyzer/Refit.Analyzer.Sample/IWrongInterface.cs
@@ -1,31 +1,19 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Refit.Analyzer.Sample;
 
-[SuppressMessage("Design", "RF001:Method either has no Refit HTTP method attribute",
-    Justification = "Method for testing")]
 public interface IWrongInterface
 {
-    [Refit.Get("AAA")]
+    [Get("AAA")]
     Task<IApiResponse> Get();
     
     [Refit.Post("BBB")]
     Task<IApiResponse> PostOne();
     
-    [Post("BBB")]
+    [FakeTestAttributes.Post("BBB")]
     Task<IApiResponse> PostTwo();
 
     [Put("/CCC")]
     Task<IApiResponse> Put();
-}
-
-
-public class PostAttribute : Attribute
-{
-    public PostAttribute(string someValue)
-    {
-        
-    }
 }
